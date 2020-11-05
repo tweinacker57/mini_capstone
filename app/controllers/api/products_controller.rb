2b3dc0fa9879b20c5f25f88ebf0b2db3af53e712
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-04 20:47:10 -0500

    Added products to database. Added info to 3 products including name, price, and description

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,6 @@
 class Api::ProductsController < ApplicationController
+  def all
+    @products = Product.all
+    render "products.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+  get "/the_products" => "products#all"
+  end
 end
