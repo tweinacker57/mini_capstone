514446e7c2ca86777bcef378be8692b776391fab
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-05 22:16:16 -0500

    added new route and new method and new json.jb

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,9 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "products.json.jb"
   end
+
+  def query
+    @message = "hello"
+    render "search.json.jb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # end
   namespace :api do
   get "/the_products" => "products#all"
+  get "/product_lookup" => "products#query"
   end
 end
