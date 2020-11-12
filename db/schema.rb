dde4a8f9f2e0084f8ef014340f06dcb8a6959075
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-11 22:25:00 -0500

    added new migration to be able to use decimals for price

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_030810) do
+ActiveRecord::Schema.define(version: 2020_11_12_031948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.integer "price"
+    t.decimal "price", precision: 8, scale: 2
     t.string "image_url"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
