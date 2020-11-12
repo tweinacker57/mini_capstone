7d494052f6e63d3ed7ce27dfe29e3aa3feec5d4e
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-11 19:29:20 -0500

    Added discount and total method to model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,16 @@ class Product < ApplicationRecord
   def tax
     price * 0.09
   end
-
   
+  def is_discounted?
+    if price < 10
+      return true
+    else
+      return false
+    end
+  end 
+
+  def total
+    price + tax
+  end
 end
