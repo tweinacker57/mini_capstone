b186dc4e7e297e172f1adbc0a1d3066b119f6b0a
Tyler Weinacker <tweinacker57@gmail.com>
2020-11-23 20:21:47 -0500

    Create has_many through associations for category.products and product.categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,8 @@ class Product < ApplicationRecord
   belongs_to :supplier
   has_many :images
   has_many :orders
+  has_many :category_products
+  has_many :categories, through: :category_products
   
   # def supplier
   #   Supplier.find_by(id: supplier_id)  
